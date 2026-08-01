6a32e170e29a4800fa3fca506f20a1d8aacd7777
Hoebat Kappa Mu <1455572+hoebat@users.noreply.github.com>
2026-08-01 10:27:56 +1000

    initialize sqlite node provider explicitly

--- rust/web/entries/sqlite-node.mjs
+++ rust/web/entries/sqlite-node.mjs
@@ -1,7 +1,9 @@
+import sqlite3InitModule from "@sqlite.org/sqlite-wasm";
 import { serveNodeProvider } from "@hara-lang/hta/provider/node";
-import { callSqlite } from "@hara-lang/db-sqlite";
+import { createSqliteProvider } from "@hara-lang/db-sqlite";
 
+const sqlite = createSqliteProvider(sqlite3InitModule);
 serveNodeProvider(
-  (operation, args) => callSqlite("node", operation, args),
+  (operation, args) => sqlite.call("node", operation, args),
   { errorCode: "db/sqlite-error" }
 );
