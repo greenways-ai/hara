002e8a377788b8462922029cf576bd48c2a62c2f
Chris Zheng <zcaudate@users.noreply.github.com>
2026-08-01 22:03:23 +1000

    adding hoplite

--- rust/examples/vm-disasm.rs
+++ rust/examples/vm-disasm.rs
@@ -8,9 +8,9 @@ fn main() {
 
 #[cfg(feature = "bytecode-vm")]
 fn main() {
-    let source = std::env::args()
-        .nth(1)
-        .unwrap_or_else(|| "(loop [i 0 acc 0] (if (< i 2500) (recur (+ i 1) ((fn [x] (+ x 1)) acc)) acc))".to_string());
+    let source = std::env::args().nth(1).unwrap_or_else(|| {
+        "(loop [i 0 acc 0] (if (< i 2500) (recur (+ i 1) ((fn [x] (+ x 1)) acc)) acc))".to_string()
+    });
     let program = hara_wasm::vm::compile_source(&source).expect("compiles");
     print!("{}", hara_wasm::vm::disassemble(&program));
 }
