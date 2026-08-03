580c958d267be7a73402ed496da6765d7cf2738d
Chris Zheng <zcaudate@outlook.com>
2026-08-04 03:17:18 +1000

    complete postgres supabase compiler helpers

--- java/src/test/java/hara/truffle/HaraNativeTestRunnerTest.java
+++ java/src/test/java/hara/truffle/HaraNativeTestRunnerTest.java
@@ -122,7 +122,7 @@ public final class HaraNativeTestRunnerTest {
       {"lib/test/postgres/core/graph_test.hal", "6", "13"},
       {"lib/test/postgres/core_public_test.hal", "5", "9"},
       {"lib/test/postgres/core/system_test.hal", "2", "3"},
-      {"lib/test/postgres/core/supabase_test.hal", "8", "25"}
+      {"lib/test/postgres/core/supabase_test.hal", "15", "42"}
     };
     for (String[] suite : suites) {
       HaraNativeTestRunner.Result result =
