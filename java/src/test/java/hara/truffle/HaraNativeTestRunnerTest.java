e2e7ecced169be91ea2b1f281a47a031e1b212ec
Chris Zheng <zcaudate@outlook.com>
2026-08-03 11:03:15 +1000

    port postgres core implementation facade

--- java/src/test/java/hara/truffle/HaraNativeTestRunnerTest.java
+++ java/src/test/java/hara/truffle/HaraNativeTestRunnerTest.java
@@ -112,7 +112,8 @@ public final class HaraNativeTestRunnerTest {
       {"lib/test/postgres/core/impl_base_test.hal", "15", "31"},
       {"lib/test/postgres/core/impl_main_test.hal", "8", "14"},
       {"lib/test/postgres/core/impl_insert_test.hal", "7", "8"},
-      {"lib/test/postgres/core/impl_update_test.hal", "7", "8"}
+      {"lib/test/postgres/core/impl_update_test.hal", "7", "8"},
+      {"lib/test/postgres/core/impl_test.hal", "4", "8"}
     };
     for (String[] suite : suites) {
       HaraNativeTestRunner.Result result =
